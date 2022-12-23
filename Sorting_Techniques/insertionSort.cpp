55893eb4f7fcbf980f79d78cf58d838639c45e81
Đào Duy An <84566480+DaoDuyAn@users.noreply.github.com>
2022-12-23 22:43:23 +0700

    Update insertionSort.cpp

--- Sorting_Techniques/insertionSort.cpp
+++ Sorting_Techniques/insertionSort.cpp
@@ -3,19 +3,19 @@ using namespace std;
 
 void insertionSort(int arr[], int n) {
     int key, j;
-    for (int i = 1; i < n; i++) {
+    for (int i = 1; i <= n - 1; i++) {
         key = arr[i];
-        j = i - 1;
+        j = i;
 
         // Move elements of arr[0..i-1],
         // that are greater than key, to one
         // position ahead of their
         // current position
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
+        while (j >= 1 and arr[j - 1] > key) {
+            arr[j] = arr[j - 1];
             j--;
         }
-        arr[j + 1] = key;
+        arr[j] = key;
     }
 }
 
